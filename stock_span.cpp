a8a5fab5d28d8cf86312b2339bde49913053aba4
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-09-19 11:56:26 +0530

    19th

--- stock_span.cpp
+++ stock_span.cpp
@@ -1,33 +1,20 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-struct ts{
-    int * arr;
-    int top1;
-    int top2;
-    int cap;
-    ts(int n){
-        arr= new int[n];
-        top1=-1;
-        top2=n;
-        cap=n;
-    }
-
-};
 void ps(int arr[], int n){
     stack<int> s;
-    s.push(0);
+    s.puch(arr[0]);
     cout<<1;
     for(int i=1; i<n;i++ ){
-        if(arr[i]<arr[s.top()]){
+        if(arr[i]<s.top()){
             cout<<i-s.top();
             s.push(i);
         }
         else{
-            while(arr[i]>arr[s.top()] && s.empty()==false){
+            while(arr[i]>s.top() && s.empty()==false){
                 s.pop();
             }
-            if(s.empty()==true){
+            if(s.empty==true){
                 cout<<i+1;
                 s.push(i);
             }
