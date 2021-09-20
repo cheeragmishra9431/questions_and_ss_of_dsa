6af7e25659bb6784d0cb99d4e3b5a0cd9902a766
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-09-20 16:18:45 +0530

    20th

--- arrays.cpp
+++ arrays.cpp
@@ -29,15 +29,62 @@ void move(int arr[], int n){
         }
     }
 }
+void stock(int arr[], int n){
+    if(n==1){
+        return ;
+    }
+    int i=0;
+    while(i<n-1){
+        if(i<n-1 &&  arr[i]>=arr[i+1] ){
+            i++;
+        }
+        if(i==n-1){
+            return ;
+        }
+        int buy= i++;
+        while (i<n && arr[i]>=arr[i-1] )
+        {
+            /* code */
+            i++;
+        }
+        int sell =i-1;
+        cout<< buy<< " "<<sell;
+        
+    }
+}
+void flips(int arr[] ,int n){
+    int k=arr[0];
+    for( int i=1; i<n ; i++){
+        if(arr[i]!=arr[i-1]){
+            if(arr[i]!=k){
+                cout<< "from" << i << 'to' ;
+
+            }
+            else{
+                cout<< i-1<<endl;
+            }
+        }
+        if(arr[n-1]!=k){
+            cout<<n-1<<endl;
+        }
+    }
+}
 int main(){
 //code;
-int * arr;
-int n;
-arr= new int[n];
-int as[]={1,2,3,4,5,6,7};
-gf(as,7);
-second_largest(as, 7);
-int x= sizeof(arr)/ sizeof(arr[0]) ;
+// int * arr;
+// int n;
+// arr= new int[n];
+// int as[]={1,2,3,4,5,6,7};
+// gf(as,7);
+// second_largest(as, 7);
+// int x= sizeof(arr)/ sizeof(arr[0]) ;
+int price[] = { 100, 180, 260, 310, 40, 535, 695 };
+int f[]={0,1,0,1,0,1,0,0,0,0};
+flips(f, 11);
+    int n = sizeof(price) / sizeof(price[0]);
+ 
+    // Function call
+    // stock(price, n);
 // x is the size of the array;
 return 0;
 }
