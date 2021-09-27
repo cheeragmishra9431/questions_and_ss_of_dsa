4725e4a2c1e137f5aca8296718a8b0ed503b44f8
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-09-27 14:04:00 +0530

    27th

---
+++ graph.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+void addE(vector<int> adj[], int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void bfs(vector<int> adj[], int s, int v){
+    bool visited[v]={false};
+    queue<int> q;
+    visited[s]=true;
+    q.push(s);
+    while(q.empty()==false){
+        int temp=q.front();
+        q.pop();
+        cout<<temp<< " ";
+        for(int i: adj[temp]){
+            if(visited[i]==false){
+                visited[i]==true;
+                q.push(i);
+            }
+        }
+    }
+}
+int main(){
+//code;
+vector<int> adj[7];
+addE(adj, 0,1);
+addE(adj, 0,2);
+addE(adj, 0,3);
+addE(adj, 0,4);
+bfs(adj, 0, 5);
+return 0;
+}
