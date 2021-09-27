6a2c60f1ce46f9ec173f8ad86f81b2e4be1fb452
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-09-27 17:08:16 +0530

    27th dfs

--- graph.cpp
+++ graph.cpp
@@ -23,6 +23,25 @@ void bfs(vector<int> adj[], int s, int v){
         }
     }
 }
+void dfsrec(vector<int> adj[], int u ,bool visited[] ){
+    visited[u]=true;
+    cout<< u<< " ";
+    for(int i: adj[u]){
+        if(visited[i]==false){
+            dfsrec(adj, i, visited);
+
+        }
+    }
+}
+void dfs(vector<int> adj[], int v){
+    bool visited[v]={false};
+    for(int i=0; i<v; i++){
+        if(visited[i]==false){
+            dfsrec(adj, i, visited);
+
+        }
+    }
+}
 int main(){
 //code;
 vector<int> adj[7];
@@ -30,6 +49,7 @@ addE(adj, 0,1);
 addE(adj, 0,2);
 addE(adj, 0,4);
 addE(adj, 0,3);
-bfs(adj, 0, 5);
+// bfs(adj, 0, 5);
+dfs(adj,5);
 return 0;
 }
