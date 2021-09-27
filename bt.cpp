85109c6611d1a32b33786aff588e8cfc5d896196
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-09-27 14:03:22 +0530

    27th

--- bt.cpp
+++ bt.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<unordered_set>
 struct Node{
     int key;
     Node* left;
@@ -19,6 +20,24 @@ void inorder(Node* root, int* i){
     arr[*i]=root->key;
     *i=*i +1;
     inorder(root->right,i);
+}
+void function_name( int arr[], int n){
+    int x[n];
+    int y[n];
+    unordered_set<int> s;
+    x[0]=0;
+    s.insert(arr[0]);
+    for(int i=1; i<n ; i++){
+        if(s.find(arr[i])!=s.end()){
+            if(s.size()!=0){
+                
+            }
+            else{
+                x[i]=0;
+            }
+        }
+        
+    }
 }
 int main(){
 //code;
